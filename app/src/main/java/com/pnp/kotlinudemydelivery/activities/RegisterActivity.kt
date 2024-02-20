d923f0e9160e9d0b51071f52dfb36d132a318b73
S1 PNP ALEXANDER MALDONADO BARRIOS <amaldonadob@policia.gob.pe>
2024-02-20 16:17:00 -0500

    Se adiciono los id para los componente y poder enviar y recibir valores a traves de las vistas

--- app/src/main/java/com/pnp/kotlinudemydelivery/activities/RegisterActivity.kt
+++ app/src/main/java/com/pnp/kotlinudemydelivery/activities/RegisterActivity.kt
@@ -3,18 +3,34 @@ package com.pnp.kotlinudemydelivery.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
 import com.pnp.kotlinudemydelivery.R
 
 class RegisterActivity : AppCompatActivity() {
 
     var imageViewGoToLogin: ImageView? = null
+    var editTextFirstName: EditText? = null
+    var editTextLastName: EditText? = null
+    var editTextEmail: EditText? = null
+    var editTextPhone: EditText? = null
+    var editTextPassword: EditText? = null
+    var editTextConfirmPassword: EditText? = null
+    var buttonRegister: Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
         imageViewGoToLogin = findViewById(R.id.imageview_go_to_login)
+        editTextFirstName = findViewById(R.id.edittext_first_name)
+        editTextLastName = findViewById(R.id.edittext_last_name)
+        editTextEmail = findViewById(R.id.edittext_email)
+        editTextPhone = findViewById(R.id.edittext_phone)
+        editTextPassword = findViewById(R.id.edittext_password)
+        editTextConfirmPassword = findViewById(R.id.edittext_confirm_password)
+        buttonRegister = findViewById(R.id.btn_register)
 
         imageViewGoToLogin?.setOnClickListener{goToLogin()}
     }
